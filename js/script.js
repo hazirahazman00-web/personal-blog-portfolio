17bdf7ab5c340b0061642e033e7050013053964e
hazirahazman00-web <hazirahazman00@gmail.com>
2026-06-03 15:28:51 +0800

    Added about blog and contact pages

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("LOTSO'S website loaded successfully.");
+
+const contactForm = document.getElementById("contactForm");
+
+if (contactForm) {
+    contactForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Thank you for contacting LOTSO'S. Your message has been received!");
+        contactForm.reset();
+    });
+}
